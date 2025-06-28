2875e511b554ce65e13fb39e1584fb5b8f4c138d
iancorbett <iancorbett1324@gmail.com>
2025-06-28 17:12:46 -0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+
+        {/*theme toggle*/}
+
+        {/*background effects*/}
+
+        {/*navbar */}
+
+        {/*main cintent*/}
+
+        {/*footer*/}
+
+
+
+    </div>
+}
