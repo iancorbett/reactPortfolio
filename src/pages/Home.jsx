5f7f0f61be879bee8fbddf4c3665c6917f593567
iancorbett <iancorbett1324@gmail.com>
2025-07-05 10:04:12 -0700

    add theme toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         {/*theme toggle*/}
-
+        <ThemeToggle />
         {/*background effects*/}
 
         {/*navbar */}
