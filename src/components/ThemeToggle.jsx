5b09dd64ecfa85a1a94bae1c773b0b714345e742
iancorbett <iancorbett1324@gmail.com>
2025-07-07 11:09:53 -0700

    add dark class

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -8,9 +8,19 @@ export const ThemeToggle = () => {
         if(isDarkMode) {
             setIsDarkMode(false)
         } else {
+            document.documentElement.classList.add("dark");
             setIsDarkMode(true)
         }
     }
 
-    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
+    return (
+    <button onClick={toggleTheme}>
+        {" "}
+        {isDarkMode ? (
+            <Sun className="h-6 w-6 text-yellow-300"/>
+         ) : (
+         <Moon className="h-6 w-6 text-blue-900"/>
+        )
+        }</button>
+    )
 }
