63f602865f82a0ca07acc4b3202ee4dc70eb88b1
iancorbett <iancorbett1324@gmail.com>
2025-07-09 10:02:32 -0700

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,9 @@ export const Navbar = () => {
         return () => window.removeEventListener("scroll", handleScroll);
 }, []);
     return (
-    <nav className={cn("fixed w-full z-40 transition-all duration-300", isScrolled)}></nav>
+    <nav className={cn("fixed w-full z-40 transition-all duration-300", 
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+    )}
+    ></nav>
     );
 }
