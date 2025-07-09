e880bab477515a29a7141dd40b30611d17f95aa8
iancorbett <iancorbett1324@gmail.com>
2025-07-09 09:59:22 -0700

    event listener

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { cn } from "../lib/utils";
 
 const navItems = [
@@ -6,8 +7,19 @@ const navItems = [
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
     {name: "Contact", href: "#contact"},
-]
+];
 
 export const Navbar = () => {
-    return <nav className={cn("fixed w-full z-40 transition-all duration-300")}></nav>;
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.screenY > 10);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+}, []);
+    return (
+    <nav className={cn("fixed w-full z-40 transition-all duration-300", isScrolled)}></nav>
+    );
 }
