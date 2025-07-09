0c164e30ceaa217cb633968152baa75d12b1b8b1
iancorbett <iancorbett1324@gmail.com>
2025-07-09 10:10:13 -0700

    navbar first span

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,15 @@ export const Navbar = () => {
     <nav className={cn("fixed w-full z-40 transition-all duration-300", 
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
     )}
-    ></nav>
+    >
+        <div className="container flex items-center justify-between">
+            <a>
+                <span>
+                    <span className="text-glow">Ian Corbett's</span> Portfolio
+                </span>
+            </a>
+
+        </div>
+    </nav>
     );
 }
