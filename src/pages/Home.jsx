efae12d256d6b94989aad9a695ea95487b5278ef
iancorbett <iancorbett1324@gmail.com>
2025-07-09 09:26:29 -0700

    IMPORT NAVBAR

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
     return (
@@ -10,7 +11,7 @@ export const Home = () => {
         {/*background effects*/}
         <StarBackground />
         {/*navbar */}
-
+        <Navbar />
         {/*main cintent*/}
 
         {/*footer*/}
