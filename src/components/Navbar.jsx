b774b0ee6fc372ea2621425c08d3e109f351f951
iancorbett <iancorbett1324@gmail.com>
2025-07-09 09:33:03 -0700

    creqte navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+
+
+
+const navItems = [
+    {name: "Home", href: "#hero"},
+    {name: "About", href: "#about"},
+    {name: "Skills", href: "#skills"},
+    {name: "Projects", href: "#projects"},
+    {name: "Contact", href: "#contact"},
+]
+
 export const Navbar = () => {
-    return <div></div>;
+    return <nav></nav>;
 }
