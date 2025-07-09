aac29e2174a04d9d4325a2adfc6f8cddccac5c8e
iancorbett <iancorbett1324@gmail.com>
2025-07-09 10:23:09 -0700

    navbar edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,21 @@ export const Navbar = () => {
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
 }, []);
-    return (
-    <nav className={cn("fixed w-full z-40 transition-all duration-300", 
+    
+return (
+    <nav 
+    className={cn(
+        "fixed w-full z-40 transition-all duration-300", 
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
     )}
     >
         <div className="container flex items-center justify-between">
-            <a>
+            <a className="text-xl font-bold text-primary flex items-center"
+            href="#hero"
+            >
                 <span className="relative z-10">
-                    <span className="text-glow">Ian Corbett's</span> Portfolio
+                    <span className="text-glow text-foreground">Ian Corbett's</span> {""}
+                    Portfolio
                 </span>
             </a>
 
