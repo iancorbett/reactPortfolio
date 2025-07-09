b656c6c53e2aea7de539e10f7056c8a22c9bc9e1
iancorbett <iancorbett1324@gmail.com>
2025-07-09 12:15:33 -0700

    navbar edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const navItems = [
 
 export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -40,12 +41,36 @@ return (
             {/*desktop nav*/}
             <div className="hidden md:flex space-x-8">
                 {navItems.map((item, key) => (
-                    <a href={item.href} key={key} className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                    <a 
+                    href={item.href} 
+                    key={key} 
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
                         {item.name}
                     </a>
                 ))}
             </div>
 
+                {/*mobile nav*/}
+                <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                )}>
+                <div className="flex flex-col space-y-8 text-xl">
+                {navItems.map((item, key) => (
+                    <a 
+                    href={item.href} 
+                    key={key} 
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    onClick={() => {
+                        setIsMenuOpen(false)
+                    }}
+                    >
+                        {item.name}
+                    </a>
+                ))}
+            </div>
+        </div>
         </div>
     </nav>
     );
