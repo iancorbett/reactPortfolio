88b074eb62e16e771b37a72cf702152f11080643
iancorbett <iancorbett1324@gmail.com>
2025-07-10 09:15:31 -0700

    styling skills

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -24,8 +24,8 @@ export const SkillsSection = () => {
     return <section id="skills" className="py-24 px-4 relative bg-secondary/30"
     >
         <div className="container mx-auto max-w-5xl">
-            <h2>
-                My <span>Skills</span>
+            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                My <span className="text-primary">Skills</span>
             </h2>
 
         </div>
