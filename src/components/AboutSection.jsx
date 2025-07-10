4af0ef3caa95eb1736e188314cbbb7c5829860de
iancorbett <iancorbett1324@gmail.com>
2025-07-09 22:37:31 -0700

    download cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,17 +10,28 @@ export const AboutSection = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                 <div className="space-y-6">
-                    <h3>
+                    <h3 className="text-2xl font-semibold">
                         Passionate web developer etc
                     </h3>
 
-                    <p>
+                    <p className="text-muted-foreground">
                         Experience andspecialization
                     </p>
 
-                    <p>
+                    <p className="text-muted-foreground">
                         Passionate about
                     </p>
+
+                    <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+                        <a href="#contact" className="cosmic-button">
+                            Get in Touch!
+                        </a>
+
+                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            Dounload CV
+                        </a>
+
+                    </div>
                 </div>
 
                 <div>
