ce65b81f66846364bc470bacdfbe3e4ff0ff4c5e
iancorbett <iancorbett1324@gmail.com>
2025-08-04 12:56:21 -0700

    create area for featured project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -34,6 +34,53 @@ const projects = [
 export const ProjectsSection = () => {
     return (
     <section id="projects" className="py-24 px-4 relative"> 
+
+{/* Featured Project */}
+<div className="mb-16">
+  <div className="group bg-card flex flex-col md:flex-row overflow-hidden rounded-lg shadow-md transition-all duration-300 hover:shadow-lg">
+    <img
+      src={featuredProject.image}
+      alt={featuredProject.title}
+      className="w-full md:w-1/2 object-cover transition-transform duration-500 group-hover:scale-105"
+    />
+    <div className="p-6 md:w-1/2 flex flex-col justify-between">
+      <div>
+        <div className="flex flex-wrap gap-2 mb-4">
+          {featuredProject.tags.map((tag, i) => (
+            <span
+              key={i}
+              className="px-2 py-1 text-xs border font-medium rounded-full bg-secondary text-secondary-foreground"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <h3 className="text-2xl font-semibold mb-2">{featuredProject.title}</h3>
+        <p className="text-muted-foreground mb-4 text-sm">
+          {featuredProject.description}
+        </p>
+      </div>
+      <div className="flex space-x-4">
+        <a
+          href={featuredProject.demoUrl}
+          target="_blank"
+          className="text-foreground/80 hover:text-primary transition-colors"
+        >
+          <ExternalLink size={20} />
+        </a>
+        <a
+          href={featuredProject.githubUrl}
+          target="_blank"
+          className="text-foreground/80 hover:text-primary transition-colors"
+        >
+          <Github size={20} />
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-primary">Projects</span></h2>
 
