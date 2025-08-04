a4ef9703125217a465d4e02d9b89deec23c59220
iancorbett <iancorbett1324@gmail.com>
2025-08-04 12:47:59 -0700

    add info about recent projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -38,7 +38,7 @@ export const ProjectsSection = () => {
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-primary">Projects</span></h2>
 
         <p className="text-center text-muted-foregroud mb-12 max-w-2xl mx-auto">
-            Here aer some projects
+        These are just a few of the projects I've developed. Check out my GitHub for even more!
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
