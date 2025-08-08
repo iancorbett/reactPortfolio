3d6a1cd407de2adcfb55a254615a2695d13307fa
iancorbett <iancorbett1324@gmail.com>
2025-08-08 10:32:23 -0700

    temporarily comment out download cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,9 +28,9 @@ export const AboutSection = () => {
                             Get in Touch!
                         </a>
 
-                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                        {/* <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Dounload CV
-                        </a>
+                        </a> */}
 
                     </div>
                 </div>
