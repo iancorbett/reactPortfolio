4caf174622ac0971d1b2bd9a3f0be53796b9d7ce
iancorbett <iancorbett1324@gmail.com>
2025-08-08 13:11:19 -0700

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Home } from "./pages/Home";
+// import { NotFound } from "./pages/NotFound";
+
+
+
+// function App() {
+  
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route index element={<Home />} />
+
+//           <Route path="*" element={<NotFound />}/>
+//           </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
-
-
 function App() {
-  
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-
-          <Route path="*" element={<NotFound />}/>
-          </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
+
