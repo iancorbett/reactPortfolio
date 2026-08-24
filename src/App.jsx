b0eb6dc405d651fd75ccc0d4fb4355761dcc0744
iancorbett <iancorbett1324@gmail.com>
2026-08-24 16:10:43 -0700

    fix hash router bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
 
 function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </HashRouter>
-  );
+  return <Home />;
 }
 
 export default App;
-
